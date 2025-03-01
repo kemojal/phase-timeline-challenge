5a93b906eef27f438adfd9d86b81ca3cc1f3a72d
kemo <kemo.jallow@hannlync.com>
2025-03-01 17:53:51 +0800

    chore: refactor input in the PlayControl to NumberInput component

--- src/NumberInput/index.tsx
+++ src/NumberInput/index.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+type InputProps = {
+  value: number;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  min: number;
+  max: number;
+  step: number;
+  "data-testid"?: string;
+};
 
-const NumberInput = () => {
+const NumberInput = ({
+  value,
+  onChange,
+  min,
+  max,
+  step,
+  "data-testid": testId,
+}: InputProps) => {
   return (
-    <div>index</div>
-  )
-}
+    <input
+      className="px-1 bg-gray-700 rounded"
+      type="number"
+      data-testid={testId}
+      min={min}
+      max={max}
+      step={step}
+      value={value}
+      onChange={onChange}
+    />
+  );
+};
 
-export default NumberInput
+export default NumberInput;
--- src/Timeline/PlayControls.tsx
+++ src/Timeline/PlayControls.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback } from "react";
+import NumberInput from "../NumberInput";
 
 type PlayControlsProps = {
   time: number;
@@ -6,44 +7,50 @@ type PlayControlsProps = {
 };
 
 export const PlayControls = ({ time, setTime }: PlayControlsProps) => {
+
   // TODO: implement time <= maxTime
 
   const onTimeChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
       setTime(Number(e.target.value));
     },
-    [setTime],
+    [setTime]
+  );
+
+  const onDurationChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setTime(Number(e.target.value));
+    },
+    [setTime]
   );
 
   return (
     <div
-      className="flex items-center justify-between border-b border-r border-solid border-gray-700 
- px-2"
+      className="flex justify-between items-center px-2 border-r border-b border-gray-700 border-solid"
       data-testid="play-controls"
     >
       <fieldset className="flex gap-1">
         Current
-        <input
-          className="bg-gray-700 px-1 rounded"
-          type="number"
-          data-testid="current-time-input"
+        
+        <NumberInput
+          value={time}
+          onChange={onTimeChange}
           min={0}
           max={2000}
           step={10}
-          value={time}
-          onChange={onTimeChange}
+          data-testid="current-time-input"
         />
       </fieldset>
       -
       <fieldset className="flex gap-1">
-        <input
-          className="bg-gray-700 px-1 rounded"
-          type="number"
-          data-testid="duration-input"
+       
+        <NumberInput
+          value={2000}
+          onChange={onDurationChange}
           min={100}
           max={2000}
           step={10}
-          defaultValue={2000}
+          data-testid="duration-input"
         />
         Duration
       </fieldset>
