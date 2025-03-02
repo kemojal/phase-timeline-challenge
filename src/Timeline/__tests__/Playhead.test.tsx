590fa9991cf41fe1867c16925e4162f9d6b4aa5b
kemo <kemo.jallow@hannlync.com>
2025-03-02 17:45:12 +0800

    test(playhead): add test for Playhead maintains its relative position during horizontal scrolling

--- src/Timeline/__tests__/Playhead.test.tsx
+++ src/Timeline/__tests__/Playhead.test.tsx
@@ -3,7 +3,7 @@ import { Playhead } from "../Playhead";
 
 
 
-describe("Playhead Behavior", () => {
+describe("Playhead Component", () => {
     const timelineWidth = 600;
 
 
@@ -14,6 +14,17 @@ describe("Playhead Behavior", () => {
         expect(playhead).toHaveStyle(`transform: translateX(calc(500px - 50%))`);
     
         rerender(<Playhead time={500} scrollLeft={100} />);
+        expect(playhead).toHaveStyle(`transform: translateX(calc(400px - 50%))`);
+      });
+
+
+      test("Playhead maintains its relative position during horizontal scrolling", () => {
+        const { rerender } = render(<Playhead time={600} scrollLeft={200} />);
+        
+        const playhead = screen.getByTestId("playhead");
+        expect(playhead).toHaveStyle(`transform: translateX(calc(400px - 50%))`);
+    
+        rerender(<Playhead time={700} scrollLeft={300} />);
         expect(playhead).toHaveStyle(`transform: translateX(calc(400px - 50%))`);
       });
 
