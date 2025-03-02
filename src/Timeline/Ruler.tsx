4149289f39a4fa4bc837c3a2743b7e0ed9f2746e
kemo <kemo.jallow@hannlync.com>
2025-03-02 14:42:10 +0800

    feat(ruler): Clicking or dragging on the Ruler updates the Current Time and Playhead position

--- src/Timeline/Ruler.tsx
+++ src/Timeline/Ruler.tsx
@@ -1,14 +1,54 @@
-export const Ruler = () => {
+import { MouseEvent, useRef } from "react";
+
+interface RulerProps {
+  time?: number;
+  setTime: (time: number) => void;
+}
+
+export const Ruler = ({
+  //  time,
+  setTime,
+}: RulerProps) => {
+  const rulerRef = useRef<HTMLDivElement | null>(null);
+  let padding = 16;
+
+  const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
+    if (e.buttons !== 1) return; // Only track when mouse is pressed
+    updateTime(e);
+  };
+
+  const handleMouseDown = (event: MouseEvent<HTMLDivElement>) => {
+    updateTime(event);
+  };
+
+  const updateTime = (e: MouseEvent<HTMLDivElement>) => {
+    if (!rulerRef.current) return;
+    const rect = e.currentTarget.getBoundingClientRect();
+    const clickX = e.clientX - rect.left - padding;
+    const scrollOffset = e.currentTarget.scrollLeft;
+    const totalX = clickX + scrollOffset;
+
+    // Round to the nearest 10
+    const roundedTime = Math.round(totalX / 10) * 10;
+    // Ensure we don't go below 0
+    const newTime = Math.max(0, roundedTime);
+
+    setTime(newTime);
+  };
   // TODO: implement mousedown and mousemove to update time and Playhead position
 
   return (
     <div
-      className="px-4 py-2 min-w-0 
-      border-b border-solid border-gray-700 
-      overflow-x-auto overflow-y-hidden"
+      ref={rulerRef}
+      className="overflow-x-auto overflow-y-hidden px-4 py-2 min-w-0 border-b border-gray-700 border-solid"
       data-testid="ruler"
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
     >
-      <div className="w-[2000px] h-6 rounded-md bg-white/25" data-testid="ruler-bar"></div>
+      <div
+        className="w-[2000px] h-6 rounded-md bg-white/25"
+        data-testid="ruler-bar"
+      ></div>
     </div>
   );
 };
--- src/Timeline/Timeline.tsx
+++ src/Timeline/Timeline.tsx
@@ -16,7 +16,7 @@ export const Timeline = () => {
       data-testid="timeline"
     >
       <PlayControls time={time} setTime={setTime} />
-      <Ruler />
+      <Ruler time={time} setTime={setTime} />
       <TrackList />
       <KeyframeList />
       <Playhead time={time} />
